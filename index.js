237337ce086218c7fa677ec8237c00074a1b27eb
moens-PC\moens <mones0008@gmail.com>
2017-01-10 18:16:00 +0600

    update data for heroku

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ var app =express();
 var storage = multer.diskStorage({
   destination: function (request, file, callback) {
     callback(null, __dirname+'/uploads');
+  },
+  filename: function (request, file, callback) {
+    console.log(file);
+    callback(null, file.originalname)
   }
-//   filename: function (request, file, callback) {
-//     console.log(file);
-//     callback(null, file.originalname)
-//   }
 });
 
 var upload = multer({storage:storage})
 
 
 
-app.set('port',8080);
+app.set('port',process.env.PORT||8080);
 app.listen(app.get('port'),function(){
     console.log('server running at '+app.get('port'));
 })
@@ -28,15 +28,6 @@ app.get('/',function(req,res){
 })
 
 app.post('/', upload.single('upload'), function (req, res, next) {
-  // req.file is the `avatar` file 
-  // req.body will hold the text fields, if there were any
-  //console.log('post called');
-  //console.log(req.file);
-  //console.log(req.body);
-  var formated = {
-      name:req.file.originalname,
-      size:req.file.size
-  }
-  //console.log(formated);
+  var formated = {name:req.file.originalname,size:req.file.size}
   res.json(formated);
 })
